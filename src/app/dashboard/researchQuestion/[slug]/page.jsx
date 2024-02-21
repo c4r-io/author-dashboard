711d6909b458c5c93d35348be6ac5e17fdc8a818
Arash Ashra <arash.ashrafnejad@gmail.com>
2024-02-21 00:52:07 -0500

    updated question options

--- src/app/dashboard/researchQuestion/[slug]/page.jsx
+++ src/app/dashboard/researchQuestion/[slug]/page.jsx
@@ -148,7 +148,7 @@ const Page = ({ params }) => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             <option value="">Choose an option</option>
-            <option value="Basic Science">Basic Science</option>
+            <option value="General Science">General Science</option>
             <option value="Clinical Science">Clinical Science</option>
             <option value="Public Health">Public Health</option>
           </select>
